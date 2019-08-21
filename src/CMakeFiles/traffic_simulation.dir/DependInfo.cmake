4764dfce88d1177426b8dc9dae01333979c49b9a
paschoeto <ricardopaschoeto@gmail.com>
2019-08-21 16:06:20 -0300

    LP:First Commit

---
+++ src/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/Graphics.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/Graphics.cpp.o"
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/Intersection.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/Intersection.cpp.o"
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/Street.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/Street.cpp.o"
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/TrafficObject.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/TrafficObject.cpp.o"
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/TrafficSimulator-L3.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/TrafficSimulator-L3.cpp.o"
+  "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/Vehicle.cpp" "/home/paschoeto/workspace/concurrency/lesson03/L3_Project/src/CMakeFiles/traffic_simulation.dir/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/traffic_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_simulation.dir/Graphics.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/Intersection.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/Street.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/TrafficObject.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/TrafficSimulator-L3.cpp.o"
+  "CMakeFiles/traffic_simulation.dir/Vehicle.cpp.o"
+  "traffic_simulation"
+  "traffic_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traffic_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
